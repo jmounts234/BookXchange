c24efc5181299a3ada907b16487a55fe07abf5f6
graham <graham.otte@gmail.com>
2015-09-23 19:13:25 -0400

    fix views and urls .py

--- bxapp/views.py
+++ bxapp/views.py
@@ -4,17 +4,20 @@ from django.template import RequestContext, loader
 
 from django.shortcuts import render_to_response
 
+def index(request):
+	return allBooks(request)
+
 def allBooks(request):
-    template = loader.get_template('books_overview.html')
-    context = RequestContext(request, {})
-    return HttpResponse(template.render(context))
+	template = loader.get_template('books_overview.html')
+	context = RequestContext(request, {})
+	return HttpResponse(template.render(context))
 
 def indiviualBooks(request):
 	template = loader.get_template('books_specview.html')
-    context = RequestContext(request, {})
-    return HttpResponse(template.render(context))
+	context = RequestContext(request, {})
+	return HttpResponse(template.render(context))
 
 def signUp(request):
 	template = loader.get_template('signup.html')
-    context = RequestContext(request, {})
-    return HttpResponse(template.render(context))
+	context = RequestContext(request, {})
+	return HttpResponse(template.render(context))
