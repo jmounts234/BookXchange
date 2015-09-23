57f08c471a510b677fc112cb4a07976ade9ac4dc
Jordan Mounts <jmounts234@gmail.com>
2015-09-23 18:42:21 -0400

    deleted todo

--- bxapp/models.py
+++ bxapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class SignUp(models.Model):
 	email = models.EmailField()
-	password = CharField(max_length=120, default='password', blank=False, null=False)
+	password = models.CharField(max_length=120, default='password', blank=False, null=False)
 
 	def __unicode__(self):
 		return self.email
