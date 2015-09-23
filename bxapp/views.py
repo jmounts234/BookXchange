baaec0c387ef79d3c4bc986e004b8ac733a99b73
Jordan Mounts <jmounts234@gmail.com>
2015-09-23 18:07:36 -0400

    Changed main page to Adrians static page

--- bxapp/views.py
+++ bxapp/views.py
@@ -5,6 +5,6 @@ from django.template import RequestContext, loader
 from django.shortcuts import render_to_response
 
 def index(request):
-    template = loader.get_template('index.html')
+    template = loader.get_template('books_overview.html')
     context = RequestContext(request, {})
     return HttpResponse(template.render(context))
