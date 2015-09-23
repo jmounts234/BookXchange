1285c48cc54e2a9e73c7a6f3adf6776ed087ba0b
Jordan Mounts <jmounts234@gmail.com>
2015-09-23 19:01:20 -0400

    mend

--- bxapp/urls.py
+++ bxapp/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.allBooks, name='allBooks'),
+    url(r'^$', views.individualBook, name='individualBook'),
+    url(r'^$', views.signUp, name='signUp'),
 ]
