e8bd157897faf11af78c71896799f47a7b8a3e75
Jordan Mounts <jmounts234@gmail.com>
2015-09-24 18:56:58 -0400

    changed url file

--- bxapp/urls.py
+++ bxapp/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-    url(r'^books_overview.html$', views.allBooks, name='allBooks'),
+    url(r'^books/$', views.allBooks, name='allBooks'),
+    url(r'^books/individual/$', views.indiviualBooks, name='indiviualBooks'),
+    url(r'^signup/$', views.signUp, name='signUp'),
+
 ]
