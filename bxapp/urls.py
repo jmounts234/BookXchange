15dd0c0ef748b9e3f3e2555d225ca269fa681569
graham <graham.otte@gmail.com>
2015-10-08 18:11:24 -0400

    clean up

--- bxapp/urls.py
+++ bxapp/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='overview'),
-    url(r'^overview/$', views.overview, name='overview'),
-    url(r'^signUp/$', views.signUp, name='signUp'),
-
+  url(r'^overview/$', views.overview, name='overview'),
+  url(r'^signUp/$', views.signUp, name='signUp')
 ]
