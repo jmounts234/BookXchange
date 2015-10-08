d5d680695e633a965219c9b9bddd49ea60256b53
graham <graham.otte@gmail.com>
2015-10-08 17:56:29 -0400

    specview -> overview

--- bxapp/urls.py
+++ bxapp/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
     url(r'^$', views.overview, name='overview'),
-    url(r'^$', views.book, name='book'),
     url(r'^signUp/$', views.signUp, name='signUp'),
 
 ]
--- bxapp/views.py
+++ bxapp/views.py
@@ -5,27 +5,27 @@ from django.shortcuts import render_to_response
 from .models import *
 
 def index(request):
-	return book(request, '9780321534965')
+	return overview(request, '9780321534965')
 	# return overview(request)
 
-def overview(request):
+# def overview(request):
 
-	# example uniqueness in book table
-	try:
-		from .helpers import createbook
-		book = createbook('9780321534965')
-		b = book.make()
-	except:
-		b = Book.objects.get(isbn='9780321534965')
+# 	# example uniqueness in book table
+# 	try:
+# 		from .helpers import createbook
+# 		book = createbook('9780321534965')
+# 		b = book.make()
+# 	except:
+# 		b = Book.objects.get(isbn='9780321534965')
 
-	template = loader.get_template('overview.html')
-	context = RequestContext(request, {'book' : b})
-	return HttpResponse(template.render(context))
+# 	template = loader.get_template('overview.html')
+# 	context = RequestContext(request, {'book' : b})
+# 	return HttpResponse(template.render(context))
 
-def book(request, isbn):
+def overview(request, isbn):
 	try:
 		book = Book.objects.get(isbn=isbn)
-		template = loader.get_template('specview.html')
+		template = loader.get_template('overview.html')
 		context = RequestContext(request, { 'book' : book })
 		return HttpResponse(template.render(context))
 	except:
