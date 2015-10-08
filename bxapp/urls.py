61e8c1c3377d87ee59130ff06ff53bc7c51607b0
Jordan Mounts <jmounts234@gmail.com>
2015-10-08 18:07:41 -0400

    fixed return to overview on signup

--- bxapp/urls.py
+++ bxapp/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
-    url(r'^$', views.overview, name='overview'),
+	url(r'^$', views.index, name='overview'),
+    url(r'^overview/$', views.overview, name='overview'),
     url(r'^signUp/$', views.signUp, name='signUp'),
 
 ]
