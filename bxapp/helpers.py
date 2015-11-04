ca1730fbd58f231c1f5828d682bd1ee11d280c21
Jordan Mounts <jmounts234@gmail.com>
2015-11-04 18:51:57 -0500

    fixed validate

--- bxapp/helpers.py
+++ bxapp/helpers.py
@@ -4,7 +4,7 @@ class createbook:
 
 	def validate_isbn(self):
 		if len(self.isbn) != 13:
-			if len(self.isbn):
+			if len(self.isbn) != 10:
 				return False 
 		for n in self.isbn:
 			if n not in "1234567890": return False
